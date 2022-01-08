d3759e6e1ea382d83ec3439c52c1d329809dd8d3
Steven Brannum <sdbrannum@gmail.com>
2022-01-07 20:10:10 -0600

    temp: working on x-axis

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { ref, Ref, onMounted, onBeforeUnmount } from 'vue';
+
+
+const truncateText = (ref: Ref<any>, width: number) => {
+    console.log('');
+}
+
 const firstXPosition = ref(0);
 let mouseDown = false;
 let mouseDownPosition = 0;
@@ -7,7 +13,6 @@ let mouseDownPosition = 0;
 const onMouseDown = (mouseEvent: MouseEvent) => { mouseDown = true; mouseDownPosition = mouseEvent.pageX; }
 const onMouseUp = (mouseEvent: MouseEvent) => { mouseDown = false; }
 const onMouseLeave = (mouseEvent: MouseEvent) => { mouseDown = false; }
-
 const onMouseMove = (mouseEvent: MouseEvent) => {
     if (mouseDown) {
         if (mouseEvent.pageX >= mouseDownPosition) {
@@ -19,8 +24,7 @@ const onMouseMove = (mouseEvent: MouseEvent) => {
     }
 }
 
-
-const scrollAreaRef = ref(null);
+const scrollAreaRef: Ref<SVGRectElement | null> = ref(null);
 const onScroll = (scrollEvent: Event) => {
     console.log('on scroll ', scrollEvent)
 }
@@ -28,16 +32,44 @@ const onScroll = (scrollEvent: Event) => {
 let last_known_scroll_position = 0;
 let ticking = false;
 
+
+interface Tick {
+    x: Number;
+}
+const ticks: Ref<Tick[]> = ref([]);
+
+const calculateXAxis = () => {
+    if (scrollAreaRef.value) {
+        setTimeout(() => { }, 1000)
+        const bodyPos = document.body.getBoundingClientRect();
+        const pos = scrollAreaRef.value.getBoundingClientRect();
+        console.log(scrollAreaRef.value.scrollLeft);
+        let w = pos.width;
+        console.log(w, pos)
+        let lastX = 150;
+        let idx = 0;
+        while (w > -300) {
+            lastX = (pos.left - bodyPos.left) + (idx++ * 100)
+            ticks.value.push({ x: lastX })
+            w -= 150;
+        }
+
+        console.log(ticks.value)
+    }
+}
+
 onMounted(() => {
     if (scrollAreaRef.value) {
-        console.log('adding scroll event');
-        (scrollAreaRef.value as SVGRectElement).addEventListener('wheel', onScroll)
+        calculateXAxis();
+
+        scrollAreaRef.value.addEventListener('wheel', onScroll);
+
     }
 });
 
 onBeforeUnmount(() => {
     if (scrollAreaRef.value) {
-        (scrollAreaRef.value as SVGRectElement).addEventListener('wheel', onScroll)
+        scrollAreaRef.value.addEventListener('wheel', onScroll);
     }
 });
 
@@ -47,9 +79,12 @@ onBeforeUnmount(() => {
     <div class="timeline-chart">
         <svg width="100%" height="100%">
             <g>
-                <text>Hello</text>
+                <text y="95">Hello</text>
                 <line x1="0" x2="100%" y1="100" y2="100" stroke="black" />
+                <text y="195">Long text man longer</text>
+                <line x1="0" x2="100%" y1="200" y2="200" stroke="black" />
             </g>
+
             <rect
                 class="chart-bounds"
                 x="150"
@@ -61,9 +96,15 @@ onBeforeUnmount(() => {
                 @mousemove="onMouseMove"
                 ref="scrollAreaRef"
             />
+            <line x1="150" x2="150" y1="0" y2="100%" stroke="black" stroke-width="2" />
             <g class="x-axis">
-                <g class="tick">
-                    <line x1="100" x2="100" y1="0" y2="95%" />
+                <g
+                    class="tick"
+                    v-for="(tick, idx) in ticks"
+                    :key="idx"
+                    :style="{ transform: `translateX(${tick.x}px)` }"
+                >
+                    <line y1="0" y2="95%" stroke="black" />
                     <text>Date</text>
                 </g>
             </g>
@@ -76,13 +117,19 @@ onBeforeUnmount(() => {
 
 <style scoped>
 .timeline-chart {
+    box-sizing: border-box;
     height: 300px;
     width: 100%;
     border-radius: 3px;
     border: 2px solid black;
 }
 
+.timeline-chart * {
+    box-sizing: border-box;
+}
+
 .chart-bounds {
+    box-sizing: border-box;
     height: 100%;
     width: calc(100% - 150px);
 }
